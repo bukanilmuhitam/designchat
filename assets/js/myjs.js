27d3b5151f29646edb8bec03727a5119227ce232
unknown <bukanilmuh@gmail.com>
2020-04-04 19:05:06 +0700

    design chat

---
+++ assets/js/myjs.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $("#content-slim").slimScroll({
+
+        height: '350px'
+
+    });
+
+
+    $("#chatBody").slimScroll({
+        width : '100%',
+        height: '430px',
+        start: 'bottom',
+
+    });
+    
+});
